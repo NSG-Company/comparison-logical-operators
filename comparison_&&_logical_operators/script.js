9807309e61e9f81fd15d728a30097639fa308bb0
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-02 02:31:43 +0200

    Add files via upload

---
+++ comparison_&&_logical_operators/script.js
@@ -0,0 +1,74 @@
+const age = 19;
+
+if (age > 19) {
+    console.log('enter!');
+}
+
+else if (age < 18) {
+    console.log('no entry!');
+}
+else if (age == 18) {
+    console.log("over 18");
+}
+else {
+    console.log("passed");
+}
+
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("you are invited");
+
+} else {
+
+    console.log("you're not invited");
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus === 'bob') {
+    console.log("you can drive home");
+} else {
+    console.log("you cannot drive home");
+}
+
+const ages = 25;
+
+if (ages > 17 && ages < 26) {
+
+    console.log("je krijgt 50% korting");
+
+} else {
+    console.log("geen korting helaas!")
+}
+
+const sarah = true;
+const bram = true;
+
+if ("sarah" > true || "bram" > true); {
+    console.log("gratis biertje");
+}
+
+const totalAmount = 100;
+
+if (totalAmount > 25 && totalAmount <= 50) {
+
+    console.log("gratis (vega)bitterballen")
+
+} else if (totalAmount > 50 && totalAmount < 100) {
+
+    console.log("gratis portie nachos")
+} else if (totalAmount >= 100) {
+
+    console.log("gratis flesje champagne")
+}
+
+
+
+
+
+
+
+
